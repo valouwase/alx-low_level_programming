a70242a69b7b94a9177093233c150bc9ddff73d0
valouwase <uwasevalo41@gmail.com>
2023-02-18 07:11:29 -0800

    101 quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Print quote to the standard error.
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",58);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",58);
 
 	return(1);
 }
