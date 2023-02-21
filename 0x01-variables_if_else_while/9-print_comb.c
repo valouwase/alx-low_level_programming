087ac93dbf5e89bce7f44d3dda6019262803acc3
valouwase <uwasevalo41@gmail.com>
2023-02-20 16:03:48 -0800

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 	int num;
 
 	for (num = 0; num <= 9; num++)
-	{	
+	{
 		putchar((num % 10) + '0');
 		if (num == 9)
-			continue;	
+			continue;
+
 		putchar(',');
 		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
+
 }
