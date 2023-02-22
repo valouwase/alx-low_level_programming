78e0cc6cde5b8465bfba6492b107434d9f4c92a0
valouwase <uwasevalo41@gmail.com>
2023-02-21 17:21:08 -0800

    changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,28 +1,26 @@
+#include <stdio.h>
 /** main -  prints the alphabet, in lowercase, followed by a new line.
  *
  * return: Always 0
  *
  */
-#include "main.h"
 
-void print_alphabet(void);
-
-int main(void)
-{
-        print_alphabet();
-
-        return 0;
-}
-
-
-void print_alphabet(void)
+void palphabet(void)
 {
         char lowerCase;
 
         for(lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
         {
-                putchar(lowerCase);
+                printf("%c",lowerCase);
         }
         putchar('\n');
 
 }
+int main(void)
+{
+        palphabet();
+
+        return 0;
+}
+
+
