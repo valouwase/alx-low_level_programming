d9baf8c82c756668097ba7b07ee8029bd63d9a89
valouwase <uwasevalo41@gmail.com>
2023-02-23 12:05:18 -0800

    more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * print_numbers - print numbers from 0 to 9.
+ *
+ * Return: a number.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	if (a >= 48 && a <= 57)
+		return (a);
+	_putchar("\n");
+}
