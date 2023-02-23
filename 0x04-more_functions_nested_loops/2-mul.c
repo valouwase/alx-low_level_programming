67152ebda99825313f34e48fe873cadc19ed0510
valouwase <uwasevalo41@gmail.com>
2023-02-23 11:55:45 -0800

    more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * mul - multiply two integers.
+ * @a: int parameter.
+ * @b: int parameter.
+ * Return: a number.
+ */
+int mul(int a, int b)
+{
+	_putchar(a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,3 +8,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(int a);
+int _putchar(int b);
