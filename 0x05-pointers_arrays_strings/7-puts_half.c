420b470f90b200e3b2e240222847c68b7d90ee76
valouwase <uwasevalo41@gmail.com>
2023-02-28 09:54:25 -0800

    half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	}
 	else if (len % 2 != 0)
 	{
-		for (i = (len - 1) / 2; str[i] != '\0'; i++)
+		for (i = (len / 2) + 1; str[i] != '\0'; i++)
 			_putchar(str[i]);
 	}
 }
