823e2eacd8bd75ab6a6e4116af4282aee774c914
valouwase <uwasevalo41@gmail.com>
2023-02-27 19:54:52 -0800

    second task on pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string.
+ * @str: str as parameter.
+ * Return: new line.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
