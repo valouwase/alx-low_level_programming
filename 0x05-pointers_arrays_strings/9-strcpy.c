5d51fc00fb20a9a7df8c0a65b0fc1c7ad53b4bb8
valouwase <uwasevalo41@gmail.com>
2023-02-28 09:02:44 -0800

    array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - function
+ * @dest: first array.
+ * @src: second array.
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	if  (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		return (dest);
+	}
+}
