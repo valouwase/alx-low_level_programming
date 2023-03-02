220da71049ec8455079ab2272357910b568f611e
valouwase <uwasevalo41@gmail.com>
2023-03-02 14:23:28 -0800

    more pointers and arrays

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,16 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[j])
+		if (s1[i] != s2[i])
 		{
-			if s1[i] < s2[i];
+			if (s1[i] < s2[i])
+			{
 				return (s1[i] - s2[i]);
-			else if s1[i] > s2[i];
+			}
+			else if (s1[i] > s2[i])
+			{
 				return (s1[i] - s2[i]);
+			}
 		}
 		else
 			return (0);
