7ed5e15ec0be8b37fd4aab49398d0a3f9eaeb40f
valouwase <uwasevalo41@gmail.com>
2023-03-01 20:37:41 -0800

    more pointers and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - function to concatenate two strings.
+ * @dest: first array.
+ * @src: second array.
+ * Return: one string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, dest_len, src_len;
+
+	dest_len = 0;
+	src_len = 0;
+	j = 0;
+	for (i = 0; dest_len[i] != '\0'; i++)
+		dest_len[i] += 1;
+	for (i = 0; src_len[i] != '\0'; i++)
+		src_len[i] += 1;
+	while (j <= src_len)
+		j++;
+		dest_len[dest_len + i] = src_len[i];
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+6 readme file
