0685eea3452d20b495488bc146d9e9ec432c688e
valouwase <uwasevalo41@gmail.com>
2023-03-01 20:39:46 -0800

    more pointers and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,6 +18,6 @@ char *_strcat(char *dest, char *src)
 		src_len[i] += 1;
 	while (j <= src_len)
 		j++;
-		dest_len[dest_len + i] = src_len[i];
+		dest_len[dest_len + j] = src_len[j];
 	return (dest);
 }
