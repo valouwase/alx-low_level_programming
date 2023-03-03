abc379a1fc38149e3ae053b8352b04eba841a8c2
valouwase <uwasevalo41@gmail.com>
2023-03-02 16:49:42 -0800

    changes

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - function to convert to uppercase.
+ * @str: parameter
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr -= 32;
+		}
+		ptr++;
+	}
+	return (str);
+}
