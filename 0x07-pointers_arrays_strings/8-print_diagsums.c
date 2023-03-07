bbb703e19525c34939bc97da09bb718177b80c46
valouwase <uwasevalo41@gmail.com>
2023-03-06 17:28:19 -0800

    seventh task

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagsums - function to print diagonals.
+ * @a: a pointer.
+ * @size: int parameter.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+	}
+	for (i = 0; j = size - 1; i < size; i++, j--)
+	{
+		sum2 += *(a + i * size + j);
+	}
+}
