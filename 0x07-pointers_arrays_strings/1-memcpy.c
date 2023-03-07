ebf2231a53520b3b51a872ea0448cb22f62ea856
valouwase <uwasevalo41@gmail.com>
2023-03-06 16:27:03 -0800

    seventh task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - function to copy memory area.
+ * @dest: destination memory area.
+ * @src: source pointer.
+ * @n: int parameter.
+ * Return: Always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_ptr = dest;
+	char *src_ptr = src;
+
+	while (n-- > 0)
+	{
+		*dest_ptr = **src_ptr++;
+	}
+	return (dest);
+}
