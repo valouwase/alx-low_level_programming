0449347f16473bf7c3c81f3ad5f991dddcb31415
valouwase <uwasevalo41@gmail.com>
2023-03-14 13:27:21 -0700

    task 2 debugg

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,10 @@ char *str_concat(char *s1, char *s2)
 	char *result;
 	size_t len1, len2;
 
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	result = malloc(len1 + len2 + 1);
