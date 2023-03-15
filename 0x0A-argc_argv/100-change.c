0d92c5ebd23c61953b15de21b8949ecaf914dc05
valouwase <uwasevalo41@gmail.com>
2023-03-15 13:56:38 -0700

    advanced argc&argv task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *
- *
- *
+ * main - a function to print minimum coins.
+ * @argc: int parameter.
+ * @argv: array of string values.
+ * Return: always 0.
  */
-int main()
+int main(int argc, char *argv[])
 {
+	int cents, coins;
+
+	coins = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+	printf("%d\n", coins);
 	return (0);
 }
