f36aef3a2db60c56bbe0fb3ad47e18f20ddaae44
valouwase <uwasevalo41@gmail.com>
2023-03-17 16:17:49 -0700

    fourth task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * array_range - function to print range btn max and min.
+ * @min: first int parameter.
+ * @max: second int parameter.
+ * Return: address of the pointer or null
+ */
+int *array_range(int min, int max)
+{
+	int i;
+	int *arr;
+	
+	if (min > max)
+	{
+		return (NULL);
+	}
+	arr  = (int *) malloc((max - min + 1) * sizeof(int));
+	if (!arr)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= max - min; i++)
+	{
+		arr[i] = min + i;
+	}
+	return (arr);
+}
