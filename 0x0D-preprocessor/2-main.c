c31a8a0d46427da81ef7df120d108be190bb3d43
valouwase <uwasevalo41@gmail.com>
2023-03-20 19:44:49 -0700

    Third task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,2 +1,10 @@
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+/**
+ * main - function to print file
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
