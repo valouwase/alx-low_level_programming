e4ad26c207dfb40a45c608246260fc2e62c15ad4
valouwase <uwasevalo41@gmail.com>
2023-03-23 08:53:24 -0700

    first task sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function to print sum of all integers.
+ * @n: const keyword
+ * Return: 0 or sum.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum, value;
+	unsigned int i;
+	
+	va_start(args, n);
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, int);
+		sum += value;
+	}
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
