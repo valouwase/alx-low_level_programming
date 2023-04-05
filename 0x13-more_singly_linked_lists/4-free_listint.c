5d7bca5e1a413f6ae2dfc5f62f52b3148ede27d0
valouwase <uwasevalo41@gmail.com>
2023-04-05 06:15:54 -0700

    changes

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list.
+ * @head: pointer to the head.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t curr_node;
+
+	while (head != NULL)
+	{
+		curr_node = head;
+		head = head->next;
+		free(curr_node);
+	}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list.
+ * @head: pointer to the head.
+ */
+void free_listint2(listint_t *head)
+{
+	listint_t curr_node;
+
+	while (head != NULL)
+	{
+		curr_node = head;
+		*head = (*head)->next;
+		free(curr_node);
+	}
+	*head = NULL;
+}
