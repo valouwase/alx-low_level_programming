f8c21a0c973c995234147c1768d84954a22b1286
valouwase <uwasevalo41@gmail.com>
2023-04-05 19:26:17 -0700

    changes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,23 +8,10 @@
  */
 listint_t *insert_node_int_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_n, *curr_node;
+	listint_t *new_n, *curr_node, *prev;
 	unsigned int j;
 
 	if (head == NULL)
-	{
-		return (NULL);
-	}
-	if (idx == 0)
-	{
-		return (add_nodeint(head, n));
-	}
-	curr_node = *head;
-	for (j = 0; j < idx - 1 && curr_node != NULL; j++)
-	{
-		curr_node = curr_node->next;
-	}
-	if (curr_node == NULL)
 	{
 		return (NULL);
 	}
@@ -32,9 +19,27 @@ listint_t *insert_node_int_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_n == NULL)
 	{
 		return (NULL);
+		new_n->n =n;
 	}
-	new_n->n = n;
+	if (idx == 0)
+	{
+		new_n->next = *head;
+		*head = new_n;
+		return (new_n);
+	}
+	prev = NULL;
+	curr_node = *head;
+	for (j = 0; j < idx - 1 && curr_node != NULL; j++)
+	{
+		prev = curr_node;
+		curr_node = curr_node->next;
+	}
+	if (j < idx)
+	{
+		free(new_n);
+		return (NULL);
+	}
 	new_n->next = curr_node->next;
-	curr_node->next = new_n;
+	prev->next = new_n;
 	return (new_n);
 }
