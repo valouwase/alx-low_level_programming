9d31f43e63b76f18b5097cbc0d5ab887868b2e61
valouwase <uwasevalo41@gmail.com>
2023-04-10 17:37:37 -0700

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_bit - function takes a pointer to an u
+ * @n: int parameter.
+ * @index: index of the int
+ * Return: 1 0r -1.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n | = (1UL << index);
+	return (1);
+}
